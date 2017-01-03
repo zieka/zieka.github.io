a20ea762abc4741cf1afe7a17ba67f3336f06c85
Kyle Scully <(none)>
2017-01-02 19:04:47 -0800

    jsonify links

--- current_site/src/app/about-me/about-me.component.ts
+++ current_site/src/app/about-me/about-me.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  private links = [
+    {
+      title : 'Linkedin - More about my resume',
+      href : 'https://www.linkedin.com/in/kylescully',
+      imgWidth : '40',
+      imgSrc : '../assets/img/linkedin.jpg',
+      imgAlt : 'Linkedin'
+    },
+    {
+      title : 'Github - More about my code',
+      href : 'https://github.com/zieka',
+      imgWidth : '40',
+      imgSrc : '../assets/img/github_1.png',
+      imgAlt : 'Github'
+    },
+    {
+      title : 'Instagram - More about my photography',
+      href : 'https://www.instagram.com/scullykns',
+      imgWidth : '30',
+      imgSrc : '../assets/img/instagram_1.png',
+      imgAlt : 'Instagram'
+    },
+    {
+      title : 'Facebook - More about my friends',
+      href : 'https://www.facebook.com/kyle.scully.31',
+      imgWidth : '30',
+      imgSrc : '../assets/img/facebook.png',
+      imgAlt : 'Facebook'
+    },
+    {
+      title : 'Send me an email',
+      href : 'mailto:scullykns@gmail.com',
+      imgWidth : '40',
+      imgSrc : '../assets/img/gmail.png',
+      imgAlt : 'Gmail'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
