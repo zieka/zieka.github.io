0bd115c29d5cb1b9f3698cbf4c560cb2bce906d3
Kyle Scully <scullykns@gmail.com>
2017-09-16 16:17:19 -0700

    added service worker and images

---
+++ site/sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: '',
+  root: '/',
+  staticFileGlobs: [
+    '/index.html',
+    '/*.bundle.js',
+    '/*.bundle.css',
+    '/assets/**/*'
+  ]
+};
